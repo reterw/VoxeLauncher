53317cd4c902d0dc178d3d7fa2089d3fd0b6c8ab
ci010 <cijhn@hotmail.com>
2019-07-16 18:30:23 +0800

    feat: dynamic fit the download

--- scripts/script.js
+++ scripts/script.js
@@ -14,116 +14,6 @@ $(document).ready(function () {
         // },
     });
 });
-// particlesJS('fullpage', {
-//     "particles": {
-//         "number": {
-//             "value": 80,
-//             "density": {
-//                 "enable": true,
-//                 "value_area": 800
-//             }
-//         },
-//         "color": {
-//             "value": "#ffffff"
-//         },
-//         "shape": {
-//             "type": "circle",
-//             "stroke": {
-//                 "width": 0,
-//                 "color": "#000000"
-//             },
-//             "polygon": {
-//                 "nb_sides": 5
-//             },
-//             "image": {
-//                 "src": "img/github.svg",
-//                 "width": 100,
-//                 "height": 100
-//             }
-//         },
-//         "opacity": {
-//             "value": 0.5,
-//             "random": false,
-//             "anim": {
-//                 "enable": false,
-//                 "speed": 1,
-//                 "opacity_min": 0.1,
-//                 "sync": false
-//             }
-//         },
-//         "size": {
-//             "value": 10,
-//             "random": true,
-//             "anim": {
-//                 "enable": false,
-//                 "speed": 80,
-//                 "size_min": 0.1,
-//                 "sync": false
-//             }
-//         },
-//         "line_linked": {
-//             "enable": true,
-//             "distance": 300,
-//             "color": "#ffffff",
-//             "opacity": 0.4,
-//             "width": 2
-//         },
-//         "move": {
-//             "enable": true,
-//             "speed": 12,
-//             "direction": "none",
-//             "random": false,
-//             "straight": false,
-//             "out_mode": "out",
-//             "bounce": false,
-//             "attract": {
-//                 "enable": false,
-//                 "rotateX": 600,
-//                 "rotateY": 1200
-//             }
-//         }
-//     },
-//     "interactivity": {
-//         "detect_on": "canvas",
-//         "events": {
-//             "onhover": {
-//                 "enable": false,
-//                 "mode": "repulse"
-//             },
-//             "onclick": {
-//                 "enable": true,
-//                 "mode": "push"
-//             },
-//             "resize": true
-//         },
-//         "modes": {
-//             "grab": {
-//                 "distance": 800,
-//                 "line_linked": {
-//                     "opacity": 1
-//                 }
-//             },
-//             "bubble": {
-//                 "distance": 800,
-//                 "size": 80,
-//                 "duration": 2,
-//                 "opacity": 0.8,
-//                 "speed": 3
-//             },
-//             "repulse": {
-//                 "distance": 400,
-//                 "duration": 0.4
-//             },
-//             "push": {
-//                 "particles_nb": 4
-//             },
-//             "remove": {
-//                 "particles_nb": 2
-//             }
-//         }
-//     },
-//     "retina_detect": true
-// });
 $('.menu .item').tab();
 $('.dropdown').dropdown();
 
@@ -150,6 +40,18 @@ function initGithubInfo() {
             $('#prerelease').css('visibility', 'hidden');
         }
 
+        switch (platform.os.family) {
+            case 'Windows':
+                $('[win]').clone().appendTo('[main]');
+                break;
+            case 'Mac':
+                $('[mac]').clone().appendTo('[main]');
+                break;
+            case 'Linux':
+                $('[linux]').clone().appendTo('[main]');
+                break;
+        }
+
         $('[win-portable]').attr('href', latest.assets.find(a => a.name.indexOf('-setup-') === -1 && a.name.endsWith('.exe')).browser_download_url);
         $('[win-zip-32]').attr('href', latest.assets.find(a => a.name.endsWith('win.zip') && a.name.indexOf('ia32') !== -1).browser_download_url);
         $('[win-zip]').attr('href', latest.assets.find(a => a.name.endsWith('win.zip') && a.name.indexOf('ia32') === -1).browser_download_url);
@@ -158,7 +60,6 @@ function initGithubInfo() {
         $('[dmg]').attr('href', latest.assets.find(a => a.name.endsWith('.dmg')).browser_download_url);
         $('[snap]').attr('href', latest.assets.find(a => a.name.endsWith('.snap')).browser_download_url);
         $('[appimage]').attr('href', latest.assets.find(a => a.name.endsWith('.AppImage')).browser_download_url);
-
 
         const { installer, portable, zip } = getLatestDownloadsByPlatform(latest.assets);
         $('#download').attr('href', installer.browser_download_url);
