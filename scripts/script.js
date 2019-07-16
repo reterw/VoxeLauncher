5cc1b67b04810a8b5a97a760cb1b14021aaf028e
ci010 <cijhn@hotmail.com>
2019-07-16 22:05:05 +0800

    fix: wrong lang assign

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 i18next.init({
     lng: navigator.language,
-    resources: { en, zh, zh_CN: cn, en_US: en },
+    resources: { en, zh, zh_CN: zh, en_US: en },
 }, function (err, t) {
     jqueryI18next.init(i18next, $);
     $('.section').localize();
